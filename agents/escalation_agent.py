433d3e235fed986603b606fbef4f427376cd2b2d
Omega Melese <omegamelese68@gmail.com>
2026-06-24 12:29:32 +0300

    make escalation to telegram group

--- agents/escalation_agent.py
+++ agents/escalation_agent.py
@@ -1,16 +1,11 @@
-from langchain_groq import ChatGroq
 from tools.telegram_tool import telegram_tool
-
-
-llm = ChatGroq(
-    model="openai/gpt-oss-20b",
-    temperature=0
-)
 
 def escalation_agent(state):
 
     escalation_data = telegram_tool(
         state["ticket_id"],
+        state["customer_id"],
+        state["category"],
         state["resolution"]
     )
 
--- tools/telegram_tool.py
+++ tools/telegram_tool.py
@@ -1,19 +1,64 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def telegram_tool(
     ticket_id,
-    reason
+    customer_id,
+    category,
+    resolution
 ):
     """
-    Mock escalation tool.
-
-    Later:
-    - Telegram
-    - Email
-    - Jira
+    Sends escalation details
+    to Telegram support group.
     """
 
+    token = os.getenv(
+        "TELEGRAM_BOT_TOKEN"
+    )
+
+    chat_id = os.getenv(
+        "TELEGRAM_CHAT_ID"
+    )
+
+    message = f"""
+🚨 SUPPORT ESCALATION
+
+Ticket ID: {ticket_id}
+
+Customer ID: {customer_id}
+
+Category: {category}
+
+Resolution Attempt:
+{resolution}
+
+Status: OPEN
+
+Assigned Team:
+Tier 2 Support
+"""
+
+    response = requests.post(
+        f"https://api.telegram.org/bot{token}/sendMessage",
+        json={
+            "chat_id": chat_id,
+            "text": message
+        }
+    )
+
+    if response.status_code == 200:
+
+        return {
+            "status": "sent",
+            "assigned_team": "Tier 2 Support"
+        }
+
     return {
-        "escalation_id": f"ESC-{ticket_id}",
-        "status": "OPEN",
-        "assigned_team": "Tier 2 Support",
-        "reason": reason
+        "status": "failed",
+        "error": response.text
     }
